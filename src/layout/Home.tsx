8a571a18aaf3227f185b6057906c5653c4e48105
MohammadJavad Karamooz <mohammadjaka83@gmail.com>
2026-08-13 19:23:22 +0330

    feat(modals): add UserInfoModal component for user profile editing

--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -1,11 +1,28 @@
-// this is for test the confirm modal.
-import ConfirmModal from "../components/modals/ConfirmModal";
+import UserInfoModal, { type User } from "../components/modals/UserInfoModal";
+//test data
 export default function Home() {
+  const testUser = {
+    name: "John Doe",
+    bio: "I am a software engineer from Tehran.",
+    location: "Tehran, Iran",
+    website: "https://my-portfolio.com",
+  };
+
+  const handleSave = (updatedData: User) => {
+    console.log("Data saved successfully:", updatedData);
+  };
+
+  const handleCancel = () => {
+    console.log("Modal closed without saving.");
+  };
+
   return (
-    <ConfirmModal
-      isOpen={true}
-      type="unfollow"
-      onConfirm={() => console.log("confirmed")}
-    />
+    <div className="flex items-center justify-center min-h-screen bg-black/10">
+      <UserInfoModal
+        user={testUser}
+        onCancel={handleCancel}
+        onSubmit={handleSave}
+      />
+    </div>
   );
 }
