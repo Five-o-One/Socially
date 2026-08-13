6e903c2b86d03e0fcfe72380177785f31d84ce38
Alireza <alireza.nourasli@gmail.com>
2026-08-13 19:31:20 +0330

    feat: navbar componenet added but not completed

---
+++ src/components/AppNavbar/AppNavbar.tsx
@@ -0,0 +1,9 @@
+export default function AppNavbar() {
+  return (
+    <div className="flex flex-row items-center justify-between w-full h-17 bg-header border border-border px-60 md:px-3">
+      <a className="text-xl font-mono font-bold" href="/">
+        Socially
+      </a>
+    </div>
+  );
+}
