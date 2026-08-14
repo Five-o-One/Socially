18c38d37412e9542ba570d2efe212e0ed15e4865
MohammadJavad Karamooz <mohammadjaka83@gmail.com>
2026-08-14 20:41:53 +0330

    fix(UserInfoNumber): simplify component by removing unused props and documentation.

--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -1,28 +1,24 @@
-import UserInfoModal, { type User } from "../components/modals/UserInfoModal";
-//test data
+import { AppIcon } from "../components";
+import { UserInfoNumber, UserInfoString } from "../components/user-info";
+
 export default function Home() {
-  const testUser = {
-    name: "John Doe",
-    bio: "I am a software engineer from Tehran.",
-    location: "Tehran, Iran",
-    website: "https://my-portfolio.com",
-  };
-
-  const handleSave = (updatedData: User) => {
-    console.log("Data saved successfully:", updatedData);
-  };
-
-  const handleCancel = () => {
-    console.log("Modal closed without saving.");
-  };
-
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black/10">
-      <UserInfoModal
-        user={testUser}
-        onCancel={handleCancel}
-        onSubmit={handleSave}
-      />
+    <div className="min-h-screen flex flex-col items-center justify-center gap-10 bg-bg">
+      <div className="flex gap-8">
+        <UserInfoNumber value={0} label="Following" />
+        <UserInfoNumber value={1} label="Posts" />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <UserInfoString
+          icon={<AppIcon nameIcon="Location" size={16} />}
+          value="No location"
+        />
+        <UserInfoString
+          icon={<AppIcon nameIcon="Link" size={16} />}
+          value="No website"
+        />
+      </div>
     </div>
   );
 }
