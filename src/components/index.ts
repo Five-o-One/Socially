5634cf5d623634b600fb34932c3b5157c5a64bc1
Shahryar Pi <dev.shahryar@gmail.com>
2026-08-14 12:55:40 +0330

    feat: add component on index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
-import AppIcon from "./AppIcon/AppIcon";
+import AppIcon from './AppIcon/AppIcon.tsx'
+import AppNavbar from './AppNavbar/AppNavbar.tsx'
 import AppSpinner from './AppSpinner/AppSpinner.tsx'
+import AppTab from './AppTab/AppTab.tsx'
+import ConfirmModal from './AppModals/ConfirmModal.tsx'
+import UserInfoModal from './AppModals/UserInfoModal.tsx'
 
-export {AppIcon , AppSpinner}
+export {AppIcon, AppNavbar , AppSpinner , AppTab , ConfirmModal , UserInfoModal}
